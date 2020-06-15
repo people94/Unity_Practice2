93539105c01a3e936e327fc66e174eeffa1bb0eb
Seung Yoon Baek <bsyu0804@naver.com>
2020-06-15 22:07:10 +0900

    1. ObjectPooling
    - 필요한 프리팹들을 미리 생성해두고 필요할때 활성화하여 사용

---
+++ UnitySkill/Assets/Skill1_ObjectPooling/Scripts/CreateCube.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateCube : MonoBehaviour
+{
+    //public GameObject m_goPrefab = null;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(CreateCoroutine());
+    }
+
+    IEnumerator CreateCoroutine()
+    {
+        while (true)
+        {
+            yield return null;
+            //Instantiate(m_goPrefab, Vector3.zero, Quaternion.identity);
+            GameObject t_object = ObjectPoolingManager.instance.GetQueue();
+            t_object.transform.position = Vector3.zero;
+        }
+    }
+}
